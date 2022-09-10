b7e9b1aadc87d528fa08585eb43a08e607d0046f
Cristhian Martínez <chrismartinezlara@outlook.es>
2022-09-10 17:23:52 -0600

    gitignore & RequestMapping First Level

--- src/main/java/com/bolsadeideas/springboot/app/web/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/app/web/controllers/IndexController.java
@@ -3,11 +3,15 @@ package com.bolsadeideas.springboot.app.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+// First Level Route
+@RequestMapping("/app")
 public class IndexController {
 
-    @GetMapping({ "/", "/index"})
+    // Second Level Route
+    @GetMapping({ "/", "", "/index"})
     public String index(Model model) {
         // Pass Data to View -> Model (Interface)
         model.addAttribute("title", "Spring Web App");
